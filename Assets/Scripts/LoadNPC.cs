99329593647941416d8b7c61f923d0e0ef92f8f4
dterbeest <dterbeest@gmail.com>
2014-08-28 11:09:45 +0200

    * added prefab for UMA loading  * added temporary script LoadNPC.cs for loading maincharacter  * added MainCharacter.txt, contains UMA data for main character  * updated scene to contain a plane, camerarig and load up UMA character at start

---
+++ Assets/Scripts/LoadNPC.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UMA;
+using com.ootii.Cameras;
+
+public class LoadNPC : BaseBehaviour {
+
+  public string txtFileName;
+  public IndAdventureRig camRig;
+
+  public override void Start() {
+    Load();
+  }
+
+  public override void OnGUI() {
+    // if (GUI.Button(new Rect(10,10,100,100), "Load NPC")) {
+      // Load();
+    // }
+  }
+
+  private void Load() {
+    GameObject go = new GameObject("Player");
+    UMAAvatarBase ua = go.AddComponent<UMADynamicAvatar>();
+    ua.context = UMAContext.FindInstance();
+
+    ua.Initialize();
+
+    string path = Application.dataPath + "/" + txtFileName;
+    var asset = ScriptableObject.CreateInstance<UMATextRecipe>();
+    asset.recipeString = System.IO.File.ReadAllText(path);
+    ua.Load(asset);
+    Destroy(asset);
+    camRig.Anchor = go.GetComponentInChildren<Locomotion>().transform;
+    // NPC npc = go.AddComponent<NPC>();
+  }
+}
