b6fd794f86d17ba443e8256fcc1be703cc8ebe19
dterbeest <dterbeest@gmail.com>
2014-09-05 14:13:28 +0200

    * added janpecs animation files

--- Assets/Scripts/CombatAnimator.cs
+++ Assets/Scripts/CombatAnimator.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public enum MoveTypes
 {
-  LeftRight,
-  RightLeft,
-  DownUp,
-  UpDown,
-  LowLeftUpRight,
-  LowRightUpLeft,
-  UpRightLowLeft,
-  UpLeftLowRight,
-  CentreStab
+  LeftRight,        //0
+  RightLeft,        //1
+  DownUp,           //2
+  UpDown,           //3
+  LowLeftUpRight,   //4
+  LowRightUpLeft,   //5
+  UpRightLowLeft,   //6
+  UpLeftLowRight,   //7
+  CentreStab        //8
 }
 
 public enum BlockAttackType
@@ -24,6 +24,7 @@ public enum BlockAttackType
 public class CombatAnimator : BaseBehaviour 
 {
 	#region Fields
+  private Animator _animator;
   #endregion
 
   #region Properties
@@ -36,9 +37,21 @@ public class CombatAnimator : BaseBehaviour
   public void StartMove(MoveTypes moveType, BlockAttackType blockAttackType)
   {
     Debug.Log("starting " + blockAttackType.ToString() + " move " + moveType.ToString());
+    if (!_animator) {
+      Debug.LogError("No animator!");
+      _animator = GetComponent<Animator>();
+      return;
+    }
+    _animator.SetInteger("AttackState", (int)moveType);
   }
   #endregion
 
   #region BaseBehaviour Methods
+  public override void Start() {
+    _animator = GetComponent<Animator>();
+    if (!_animator) {
+      Debug.LogError("No animator!");
+    }
+  }
   #endregion
 }
